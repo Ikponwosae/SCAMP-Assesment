d3adf5e6056a808fd6583ecd1641e1d72384252f
Ikponwosae <64660734+Ikponwosae@users.noreply.github.com>
2020-05-01 21:34:40 +0100

    Create fibbonaci.js

---
+++ fibbonaci.js
@@ -0,0 +1,15 @@
+function fib(n){
+	var arg = [0, n];
+
+	while(n <= 200){
+
+		var b = arg[arg.length - 1] + arg[arg.length - 2];
+		arg.push(b);
+		n = arg[arg.length - 1];
+	}
+	return arg;
+}
+
+//calling the fib function
+
+  console.log(fib(4));
